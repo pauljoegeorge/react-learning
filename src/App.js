dffa55875e3575820f41c13a740d7185575ae2a0
pauljoegeorge <pauljoegeorge@gmail.com>
2020-11-11 14:07:39 +0900

    adding person css, inline style, dynamic username and button event

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './Person/Person.css';
 import React, {useState } from 'react';
 import Person from './Person/Person';
 
@@ -22,13 +23,13 @@ const App = () => {
   console.log(personState, testState);
 
   //update person state and testState on button click
-  const swtichNameHandler = () => { 
+  const swtichNameHandler = (username) => { 
     console.log("button clicked");
     // this.state.persons[0].name = "PJOE George" ==> DONT Do this
     // setPersonState will replace personState array
     setPersonState({
       persons: [
-        {name: 'Paul Joe George', age: 20},
+        {name: username, age: 20},
         {name: 'PJoe', age: 25},
         {name: 'Joe', age: 27},
       ],
@@ -38,13 +39,45 @@ const App = () => {
     setTestState('Updated Test State');
   }
 
+  const nameChangeHandler = (event) => {
+    setPersonState({
+      persons: [
+        {name: event.target.value, age: 20},
+        {name: 'PJoe', age: 25},
+        {name: 'Joe', age: 27},
+      ]
+    })
+  }
+
+  const buttonStyle = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1x solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  }
+
   return (
     <div className="App">
       <h1>Hello I'm new to React !!</h1>
-      <button onClick={swtichNameHandler}>Test Button</button>
-      <Person name={personState.persons[0].name} age={personState.persons[0].age}>I should also come to screen</Person>
-      <Person name={personState.persons[1].name} age={personState.persons[1].age}/>
-      <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
+      <button style={buttonStyle} onClick={() => swtichNameHandler("Sagar Alias Jacky")}>Click Me</button>   {/* inefficient way*/}
+      {/* Alternative way of doing  */}
+      {/* <button onClick={swtichNameHandler.bind(this, "Sagar Alias Jacky")}>Test Button</button> */}
+      <Person
+       name={personState.persons[0].name}
+       age={personState.persons[0].age}
+       click={swtichNameHandler.bind(this, "Paul Joe George")}
+       changeHandler={nameChangeHandler}
+       >I should also come to screen
+      </Person>
+      <Person
+       name={personState.persons[1].name} 
+       age={personState.persons[1].age}
+      />
+      <Person 
+       name={personState.persons[2].name}
+       age={personState.persons[2].age}
+      />
     </div>
   );
   // return React.createElement('div', {className: 'App'},  React.createElement('h1', {className: 'App'}, 'Iam new here!!'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const person = (props) => {
 return (
-        <div>
-        <p> I am {props.name} and I am {props.age} years old !!. My Lucky number is {Math.floor(Math.random() * 30 )} </p>
+        <div className="Person">
+        <p onClick={props.click}> I am {props.name} and I am {props.age} years old !!. My Lucky number is {Math.floor(Math.random() * 30 )} </p>
         <p>{props.children}</p>  {/* props.children will  display messages within Person tag */}
+        <input type="text" onChange={props.changeHandler} value={props.name}></input>
         </div>
     )
 }
