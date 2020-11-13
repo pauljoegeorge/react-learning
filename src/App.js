b79d8228765a708c83937a949de79b8356976300
pauljoegeorge <pauljoegeorge@gmail.com>
2020-11-13 15:41:51 +0900

    adding radium , dynamic components and css library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import './Person/Person.css';
+// import Radium, {StyleRoot} from 'radium'; // for styling
 import React, {useState } from 'react';
 import Person from './Person/Person';
+import styled from 'styled-components';
 
 // FUNCTION BASED COMPONENTS
 const App = () => {
@@ -15,6 +17,7 @@ const App = () => {
           {name: 'Joe', age: 26},
         ],
         otherState: 'test',
+        showOthers: false,
   });
 
   // a string testState
@@ -33,59 +36,157 @@ const App = () => {
         {name: 'PJoe', age: 25},
         {name: 'Joe', age: 27},
       ],
-      otherState: personState.otherState
+      otherState: personState.otherState,
+      showOthers: true,
     })
 
     setTestState('Updated Test State');
   }
 
-  const nameChangeHandler = (event) => {
+  const nameChangeHandler = (event, index) => {
+    let personsList = [...personState.persons]
+    const personToBeUpdated = {...personState.persons[index]}
+    personToBeUpdated.name = event.target.value;
+    personsList[index] = personToBeUpdated
+
     setPersonState({
-      persons: [
-        {name: event.target.value, age: 20},
-        {name: 'PJoe', age: 25},
-        {name: 'Joe', age: 27},
-      ]
+      persons: personsList,
+      otherState: 'test',
+      showOthers: true,
     })
+    // setPersonState({
+    //   persons: [
+    //     {name: event.target.value, age: 20},
+    //     {name: 'PJoe', age: 25},
+    //     {name: 'Joe', age: 27},
+    //   ],
+    //   otherState: 'test',
+    //   showOthers: true,
+    // })
   }
 
   const buttonStyle = {
-    backgroundColor: 'white',
+    backgroundColor: 'green',
+    color: 'white',
     font: 'inherit',
     border: '1x solid blue',
     padding: '8px',
-    cursor: 'pointer'
+    cursor: 'pointer',
+    ':hover':{
+      backgroundColor: 'lightgreen',
+      color: 'black'
+    }
+  }
+
+  const PersonListToggleHandler = () => {
+    // update show others status
+    const currentShowPersonStatus = personState.showOthers;
+    setPersonState({
+      persons: [
+        {name: 'Paul', age: 20},
+        {name: 'PJoe', age: 25},
+        {name: 'Joe', age: 26},
+      ],
+      otherState: 'test',
+      showOthers: !currentShowPersonStatus
+    });
+
+    // swtichNameHandler("Sagar Alias Jacky")
+  }
+
+  const deletePersonHandler = (index) => {
+    // copy persons list to personsList const by using splice
+    const personsList = [...personState.persons]
+    personsList.splice(index, 1);
+    setPersonState({
+      persons: personsList,
+      otherState: 'test',
+      showOthers: true,
+    })
+  }
+
+  let personsList = null;
+  if(personState.showOthers){
+    personsList = (
+      <div>
+          {personState.persons.map((person, index) => {
+            return <Person
+                    key={index} // pass unique value (eg: pass unique ID)
+                    click={() => deletePersonHandler(index)}
+                    changeHandler={(event) => nameChangeHandler(event, index)}
+                    name={person.name}
+                    age={person.age}
+            />
+          })}
+          {/* <Person
+          name={personState.persons[0].name}
+          age={personState.persons[0].age}
+          click={swtichNameHandler.bind(this, "Paul Joe George")}
+          changeHandler={nameChangeHandler}
+          >I should also come to screen
+          </Person>
+          <Person
+          name={personState.persons[1].name} 
+          age={personState.persons[1].age}
+          />
+          <Person 
+          name={personState.persons[2].name}
+          age={personState.persons[2].age}
+          /> */}
+      </div>
+    )
+    buttonStyle.backgroundColor='red';
+    buttonStyle[':hover'] = {
+      backgroundColor: 'orange',
+      color: 'white'
+    }
+  }
+
+  // available css classes
+  let cssClasses = [];
+  if(personState.persons.length <= 2){
+    cssClasses.push('red'); 
+  }
+
+  if(personState.persons.length <= 1){
+    cssClasses.push('bold');
   }
 
   return (
+    // <StyleRoot>
     <div className="App">
-      <h1>Hello I'm new to React !!</h1>
-      <button style={buttonStyle} onClick={() => swtichNameHandler("Sagar Alias Jacky")}>Click Me</button>   {/* inefficient way*/}
+      <h1 className={cssClasses}>Hello I'm new to React !!</h1>
+      <button style={buttonStyle} onClick={PersonListToggleHandler}>Toggle Person List</button>   {/* inefficient way*/}
+      {/* <button style={buttonStyle} onClick={() => swtichNameHandler("Sagar Alias Jacky")}>Click Me</button>   inefficient way */}
       {/* Alternative way of doing  */}
       {/* <button onClick={swtichNameHandler.bind(this, "Sagar Alias Jacky")}>Test Button</button> */}
-      <Person
-       name={personState.persons[0].name}
-       age={personState.persons[0].age}
-       click={swtichNameHandler.bind(this, "Paul Joe George")}
-       changeHandler={nameChangeHandler}
-       >I should also come to screen
-      </Person>
-      <Person
-       name={personState.persons[1].name} 
-       age={personState.persons[1].age}
-      />
-      <Person 
-       name={personState.persons[2].name}
-       age={personState.persons[2].age}
-      />
+      {/* { personState.showOthers ?
+        <div>
+          <Person
+          name={personState.persons[0].name}
+          age={personState.persons[0].age}
+          click={swtichNameHandler.bind(this, "Paul Joe George")}
+          changeHandler={nameChangeHandler}
+          >I should also come to screen
+          </Person>
+          <Person
+          name={personState.persons[1].name} 
+          age={personState.persons[1].age}
+          />
+          <Person 
+          name={personState.persons[2].name}
+          age={personState.persons[2].age}
+          />
+        </div> : null
+      } */}
+      {personsList}
     </div>
+    /* </StyleRoot> */
   );
-  // return React.createElement('div', {className: 'App'},  React.createElement('h1', {className: 'App'}, 'Iam new here!!'));
-  
+  //return React.createElement('div', {className: 'App'},  React.createElement('h1', {className: 'App'}, 'Iam new here!!')); 
 }
-
+// export default Radium(App);
 export default App;
-
 
 // CLASS BASED COMPONENTS
 // class App extends Component {
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+// import Radium, {StyleRoot}  from 'radium';
+
+const StyleDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+    
+    @media (min-width: 500px) {
+        width: 400px;
+    }
+`;
 
 const person = (props) => {
-return (
-        <div className="Person">
-        <p onClick={props.click}> I am {props.name} and I am {props.age} years old !!. My Lucky number is {Math.floor(Math.random() * 30 )} </p>
-        <p>{props.children}</p>  {/* props.children will  display messages within Person tag */}
-        <input type="text" onChange={props.changeHandler} value={props.name}></input>
-        </div>
+    // const style = {
+    //     '@media (min-width: 500px)': {
+    //         width: '60%'
+    //     }
+    // }
+    return (
+        // <StyleRoot>
+        <StyleDiv>
+            {/* <div className="Person"> */}
+            <p onClick={props.click}> I am {props.name} and I am {props.age} years old !!. My Lucky number is {Math.floor(Math.random() * 30 )} </p>
+            <p>{props.children}</p>  {/* props.children will  display messages within Person tag */}
+            <input type="text" onChange={props.changeHandler} value={props.name}></input>
+            {/* </div> */}
+        </StyleDiv>
+        //</StyleRoot>
     )
 }
 
+// export default Radium(person);
 export default person;
